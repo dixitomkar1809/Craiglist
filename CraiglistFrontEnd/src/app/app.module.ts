521fbc1e46f55f8de2130ba64f10fa45baf1a25f
Tushar Chemburkar <chemburkartushar@gmail.com>
2018-04-07 03:53:25 -0500

    changed component structure

--- CraiglistFrontEnd/src/app/app.module.ts
+++ CraiglistFrontEnd/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { IndexComponent } from './index/index.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'index', component: IndexComponent }];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LandingPageComponent,
+    IndexComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
