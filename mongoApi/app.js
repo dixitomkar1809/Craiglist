db837257534faa4fe4165b76370aac522e5a10e9
Tushar Chemburkar <chemburkartushar@gmail.com>
2018-04-13 04:33:12 -0500

    Mongo api addede

--- mongoApi/app.js
+++ mongoApi/app.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var monk = require('monk');
+var mongo = require('mongodb');
 
 // create express app
 var app = express();
 
 // parser requests of content-type
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type
 app.use(bodyParser.json());
 
 // define a simple route
 app.get('/', function (req, res) {
-    res.json({"message": "Connected to the MongoDb Nodejs API"});
+    res.json({ "message": "Connected to the MongoDb Nodejs API" });
 });
 
 // listen for requests
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is listening to port 3000");
 });
 
@@ -29,53 +30,78 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url);
 
-mongoose.connection.on('error', function(){
+mongoose.connection.on('error', function () {
     console.log('Could not connect to the database . Exiting now....');
     process.exit();
 });
 
-mongoose.connection.once('open', function(){
+mongoose.connection.once('open', function () {
     console.log('Successfully connected to the database');
 });
 
 var db = monk('localhost:27017/Craiglist');
 
 // get all Services
-app.get('/api/service/all', function(request, result){
-    var collection = db.get ('service');
-    collection.find({}, function(err, services){
+app.get('/api/service/all', function (request, result) {
+    var collection = db.get('Service');
+    collection.find({}, function (err, services) {
         if (err) throw err;
         result.json(services);
     });
 });
 
 // get all users
-app.get('/api/user/all', function(request, result){
+app.get('/api/user/all', function (request, result) {
     var collection = db.get('users');
-    collection.find({}, function(err, users){
+    collection.find({}, function (err, users) {
         if (err) throw err;
         result.json(users);
     });
 });
 
+// get specific user
+app.get('/api/user/:useremail/:password', function (request, result) {
+    var collection = db.get('users');
+    collection.find({
+        email: request.params.useremail,
+        password: request.params.password
+    },
+        function (err, users) {
+            if (err) throw err;
+            result.json(users);
+        });
+});
+
 // get all service category
-app.get('/api/serviceCategory/all', function(request, result){
+app.get('/api/serviceCategory/all', function (request, result) {
     var collection = db.get('serviceCategory');
-    collection.find({}, function(err, categories){
+    collection.find({}, function (err, categories) {
         if (err) throw err;
         result.json(categories);
     });
 });
 
 // get service by search
-app.get('/api/service/:searchInput', function(request, result){
-    var collection= db.get('service');
-    collection.find({}, function(err, services){
+app.get('/api/service/:searchInput', function (request, result) {
+    var collection = db.get('Service');
+    var value = request.params.searchInput;
+    var pattern = "^value"
+    // console.log(value);
+    collection.find({ "serviceName": value }, function (err, services) {
         if (err) throw err;
         result.json(services);
     });
 });
 
 // get service by category
+app.get('/api/serviceCategory/:servicecategory', function (request, result) {
+    var collection = db.get('Service');
+    collection.find({
+        serviceCategory: request.params.servicecategory
+    }, function (err, categories) {
+        if (err) throw err;
+        result.json(categories);
+    });
+});
 
 // 
